a7048ebc97454a1655938bfcc3659ddf1f708a35
Simeon <aleksov_s@outlook.com>
2021-06-23 17:26:20 +0200

    Pushes the image to dockerhub

--- controller/docker-controller.go
+++ controller/docker-controller.go
@@ -9,17 +9,22 @@ import (
   "errors"
   "log"
   "time"
+  "encoding/base64"
+  "strings"
   "context"
   "github.com/go-git/go-git/v5"
   "github.com/mitchellh/go-homedir"
   "github.com/docker/docker/api/types"
   "github.com/docker/docker/client"
   "github.com/docker/docker/pkg/archive"
+  "github.com/joho/godotenv"
   "github.com/gin-gonic/gin"
   "github.com/Shigoto-Q/docker_service/service"
   "github.com/Shigoto-Q/docker_service/entity"
 
 )
+var dockerRegistryUserID = "shigoto"
+
 type DockerController interface {
   Save(ctx *gin.Context, cli *client.Client) entity.DockerImage
 }
@@ -68,6 +73,38 @@ func dprint(rd io.Reader) error {
   return nil
 }
 
+func imagePush(cli *client.Client, imageName string) error {
+    ctx, cancel := context.WithTimeout(context.Background(), time.Second*120)
+    defer cancel()
+    err := godotenv.Load(".env")
+    if err != nil {
+      fmt.Println(err)
+    }
+    user := os.Getenv("DOCKERHUBUSER")
+    token := os.Getenv("DOCKERHUBTOKEN")
+
+    var authConfig = types.AuthConfig{
+      Username: user,
+      Password: token, 
+      ServerAddress: "https://index.docker.io/v1/",
+    }
+    authConfigBytes, _ := json.Marshal(authConfig)
+    authConfigEncoded := base64.URLEncoding.EncodeToString(authConfigBytes)
+    tag := dockerRegistryUserID + imageName
+    opts := types.ImagePushOptions{RegistryAuth: authConfigEncoded}
+    rd, err := cli.ImagePush(ctx, tag, opts)
+    if err != nil {
+      return err
+    }
+  
+    defer rd.Close()
+    err = dprint(rd)
+    if err != nil {
+      return err
+    }
+    return nil
+}
+
 func GetContext(filePath string) io.Reader {
     filePath, err := homedir.Expand(filePath)
     if err != nil {
@@ -79,20 +116,19 @@ func GetContext(filePath string) io.Reader {
 
 
 
-func imageBuild(cli *client.Client, filePath string) error {
+func imageBuild(cli *client.Client, filePath string, imageName string) error {
     ctx, cancel := context.WithTimeout(context.Background(), time.Second*120)
     defer cancel()
 
     opts := types.ImageBuildOptions{
         Dockerfile: "Dockerfile",
-        Tags:       []string{"workpls"},
+        Tags:       []string{dockerRegistryUserID + imageName},
         Remove:     true,
     }
     res, err := cli.ImageBuild(ctx, GetContext(filePath), opts)
     if err != nil {
         return err
     }
-
     defer res.Body.Close()
     err = dprint(res.Body)
     if err != nil {
@@ -121,10 +157,23 @@ func (c *controller) Save(ctx *gin.Context, cli *client.Client) entity.DockerIma
   if err != nil {
       log.Fatal(err)
   }
-  err = imageBuild(cli, "/tmp/" + name)
+  imageNameS := strings.Split(name, "/")
+  imageName :=  "/" + imageNameS[len(imageNameS)-1]
+  err = imageBuild(cli, "/tmp/" + name, imageName) 
   if err != nil {
     fmt.Println(err.Error())
   }
+  
+  err = imagePush(cli, imageName)
+  if err != nil {
+    fmt.Println(err.Error())
+  }
+  docker.ImageName = dockerRegistryUserID + imageName
+  err = os.RemoveAll("/tmp/" + name)
+  if err != nil {
+      fmt.Println(err.Error())
+  }
+
   return docker
 
 }
--- entity/docker_service.go
+++ entity/docker_service.go
@@ -5,4 +5,5 @@ package entity
 type DockerImage struct {
   RepoUrl string `json:"repo_url"`
   FullName string `json:"full_name"`
+  ImageName string `json:"image_name"`
 }
